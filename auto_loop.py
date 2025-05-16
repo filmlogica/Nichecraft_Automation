4b85c074fda2a141a178dff7c8ebe4e7e1f2ebca
filmlogica <filmlogica@gmail.com>
2025-05-15 22:18:35 -0400

    Update auto_loop.py

--- auto_loop.py
+++ auto_loop.py
@@ -1,1 +1,9 @@
+import time
+from main import run_product_generation
 
+if __name__ == '__main__':
+    print("📆 ShopiGenie automation started. Press CTRL+C to stop.")
+    while True:
+        run_product_generation()
+        print("⏳ Waiting 6 hours...")
+        time.sleep(6 * 60 * 60)  # 6 hours
